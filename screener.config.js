893466bb419fbc076c0eeac716c96d8c5e3c96c1
senthil <senthil.dhandapani@ie.com.au>
2021-06-07 10:10:09 +1000

    template

---
+++ screener.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    projectRepo: 'https://github.com/chromaui/intro-storybook-react-template',
+    storybookConfigDir: '.storybook',
+    apiKey: '59d54229-4794-4661-afe6-a8a3fe8a2914',
+    resolution: '1024x768'
+  };
